0d1948179154c1ddc114b388f42851fc939ffcf5
isaac-martin <isaac@weareplayground.co>
2018-05-29 21:20:52 -0400

    Video 18

--- jiffy/src/App.js
+++ jiffy/src/App.js
@@ -1,18 +1,73 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import loader from './images/loader.svg';
+
+const Header = () => (
+  <div className="header grid">
+    <h1 className="title">Jiffy</h1>
+  </div>
+);
+
+const Userhint = ({loading, hintText}) => (
+  <div className="user-hint">
+    {loading ? <img className="block mx-auto" src={loader} /> : hintText}
+  </div>
+);
 
 class App extends Component {
+  searchGiphy = async searchTerm => {
+    try {
+      const response = await fetch(
+        'https://api.giphy.com/v1/gifs/search?api_key=lNddjF93W1e1puBzOFqqDPiZBGd3utLP&q=Dog&limit=25&offset=0&rating=R&lang=en'
+      );
+
+      const data = await response.json();
+
+      console.log(data);
+    } catch (error) {}
+  };
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchTerm: ''
+    };
+  }
+  handleChange = event => {
+    const {value} = event.target;
+    this.setState((prevState, props) => ({
+      ...prevState,
+      searchTerm: value,
+      hintText: value.length > 2 ? `Hit Enter To Search ${value}` : ''
+    }));
+  };
+
+  //when we have two or more in our search box and we press enter
+  // we want to run a search
+
+  handleKeyPress = event => {
+    const {value} = event.target;
+
+    if (value.length > 2 && event.key === 'Enter') {
+      this.searchGiphy(value);
+    }
+  };
+
   render() {
+    const {searchTerm} = this.state;
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="page">
+        <Header />
+        <div className="grid search">
+          {}
+          <input
+            className="grid-item input"
+            placeholder="Type Something"
+            onChange={this.handleChange}
+            onKeyPress={this.handleKeyPress}
+            value={searchTerm}
+          />
+        </div>
+
+        <Userhint {...this.state} />
       </div>
     );
   }
